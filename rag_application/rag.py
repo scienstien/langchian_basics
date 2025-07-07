d66fceb3bb2f23b41cb33deb59d89016357fbf5e
Scienstien <chayan23rd@gmail.com>
2025-07-07 21:55:04 +0530

    Third commit

--- rag_application/rag.py
+++ rag_application/rag.py
@@ -8,6 +8,7 @@ from langchain.vectorstores import FAISS
 from langchain_community.llms.huggingface_pipeline import  HuggingFacePipeline
 from langchain.memory import ConversationBufferMemory
 from langchain.chains.llm import LLMChain
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 import os
 from langchain.chains import RetrievalQA, LLMChain
@@ -19,7 +20,9 @@ def Vector_stores(text_chunks):
     db = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return db
 
-def get_conversation_chain(db, query , k=3):
+# Use this version of the function if you want to use the local model
+# 
+# def get_conversation_chain(db, query , k=3):
     docs = db.similarity_search(query, k=k)
     docs_text = "".join([doc.page_content for doc in docs])
     local_pipeline = pipeline("text2text-generation", model="google/flan-t5-base")
@@ -46,6 +49,32 @@ def get_conversation_chain(db, query , k=3):
     })
     response = response.replace('\n', ' ')
     return response
+
+
+def get_conversation_chain(db, query , k=3):
+
+
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.7, api_key=os.getenv("GOOGLE_API_KEY"))  # Store the API key in the .env file
+    docs = db.similarity_search(query, k=k)
+    docs_text = "".join([doc.page_content for doc in docs])
+    prompt = PromptTemplate(
+        input_variables= ['question', 'context'],
+        template = """
+        "You are a helpful and honest assistant. Use the provided context to answer the user's question.\n"
+        "If the answer is not contained within the context, just say you don't know. Do not make up an answer.\n\n"
+        "Context:\n{context}\n\n"
+        "Question:\n{question}\n\n"
+        "Answer:"
+        """
+    )
+  
+    chain = LLMChain(llm=llm, prompt=prompt, verbose=True)
+    response = chain.run({
+        'question': query,
+        'context': docs_text
+    })
+    return response, f"\n Source is {docs_text.strip()}"
+
 
 def get_pdf_text(pdf_docs):
     text = ''
